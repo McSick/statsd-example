698f5d21de1389af47ba72d1dd6f2c45bb719c5f
Michael Sickles <michaelsickles@honeycomb.io>
2024-11-15 13:33:21 -0500

    metrics-as-attributes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const HotShot = require('hot-shots');
 const opentelemetry = require('@opentelemetry/api');
 
 const myMeter = opentelemetry.metrics.getMeter(
@@ -9,47 +8,60 @@ const myMeter = opentelemetry.metrics.getMeter(
 const app = express();
 const port = 4000;
 
-// Initialize HotShot client
-const client = new HotShot({
-  host: 'localhost',
-  port: 8125, // Default StatsD port
-  prefix: 'statsd.' // Optional prefix for all metrics
-});
-const requests = myMeter.createCounter('requests');
-const histogram = myMeter.createHistogram('my_histogram');
-const timing = myMeter.createHistogram('response_time');
-const distribution = myMeter.createHistogram('my_distribution');
-const guage = myMeter.createGauge('my_guage');
-
 // Middleware to count requests
 app.use((req, res, next) => {
-  client.increment('requests');
-  requests.add(1);
-
   next();
 });
-
-// Example route
-app.get('/', (req, res) => {
-  // Record response time
-  const start = Date.now();
-  
-  // Simulate some work
+const requests = myMeter.createCounter('metric.requests.attempt');
+const requests_success = myMeter.createCounter('metric.requests.success');
+const requests_failure = myMeter.createCounter('metric.requests.failures');
+app.get('/counter', (req, res) => {
   setTimeout(() => {
-    const duration = Date.now() - start;
-    client.timing('response_time', duration);
-    timing.record(duration);
-    client.histogram('my_histogram', 42);
-    histogram.record(42);
-    client.distribution('my_distribution', 50);
-    distribution.record(50)
-    client.gauge('my_gauge', 123.45);
-    guage.record(123.45)
-    
-    res.send('Hello World!');
+    let activeSpan = opentelemetry.trace.getActiveSpan();
+    requests.add(1); // metric
+    activeSpan.setAttribute('span.requests.attempt', 1); // metric as span attribute
+    // simulate some failures 10% of the time
+    if (Math.random() > 0.9) {
+      requests_failure.add(1); // metric
+      activeSpan.setAttribute('span.requests.failures', 1); // metric as span attribute
+      return res.status(500).send('Failed!');
+    } else {
+
+      requests_success.add(1); //metric 
+      activeSpan.setAttribute('span.requests.success', 1); // metric as span attribute
+      activeSpan.setAttribute('error', true); //use any attribute for group by adhoc metrics
+      return res.send('Hello World!');
+    }
+  }, 100);
+});
+const bytes_histogram = myMeter.createHistogram('metric.response.size', {
+  description: 'The bytes of the response',
+  unit: 'bytes'
+});
+app.get('/histogram', (req, res) => {
+  setTimeout(() => {
+    let activeSpan = opentelemetry.trace.getActiveSpan();
+    let randomBytes = Math.random() * 100;
+    bytes_histogram.record(randomBytes); //
+    activeSpan.setAttribute('span.response.size',randomBytes); // metric as span attribute & desired state
+    return res.send('Histogram!');
   }, 100);
 });
 
+const memory_guage = myMeter.createGauge('metric.memory_in_use', {
+  description: 'The bytes in use',
+  unit: 'bytes'
+});
+app.get('/guage', (req, res) => {
+  setTimeout(() => {
+    let activeSpan = opentelemetry.trace.getActiveSpan();
+    // simulate memory usage betwee 60-100
+    let simulatedMemory = Math.random() * 40 + 60;
+    memory_guage.record(simulatedMemory); // metric
+    activeSpan.setAttribute('span.memory_in_use',simulatedMemory); // metric as span attribute & desired state
+    return res.send('Guage!');
+  }, 100);
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- instrumentation.js
+++ instrumentation.js
@@ -2,47 +2,29 @@
 'use strict';
 
 const opentelemetry = require('@opentelemetry/sdk-node');
-const api = require('@opentelemetry/api');
 const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-http');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http')
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { Resource } = require('@opentelemetry/resources');
-const { DropAggregation, View } = require('@opentelemetry/sdk-metrics');
-const {
-    ATTR_SERVICE_NAME,
-    ATTR_SERVICE_VERSION,
-  } = require('@opentelemetry/semantic-conventions');
-const resource = Resource.default().merge(
-    new Resource({
-      [ATTR_SERVICE_NAME]: 'sdk-metrics',
-      [ATTR_SERVICE_VERSION]: '0.1.0',
-    }),
-  );
 const {
     MeterProvider,
     PeriodicExportingMetricReader,
   } = require('@opentelemetry/sdk-metrics');
-
-  const metricReader = new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter(),
-  
-    // Default is 20000ms (20 seconds). Set to 20 seconds for demonstrative purposes only.
-    exportIntervalMillis: 20000,
-  });
-  const dropHTTPMetricView = new View({
-    aggregation: new DropAggregation(),
-    // apply the view to all instruments
-    meterName: '@opentelemetry/instrumentation-http',
-  });
-  const myServiceMeterProvider = new MeterProvider({
-    resource: resource,
-    readers: [metricReader],
-    views: [dropHTTPMetricView]
-  });
-  // Set this MeterProvider to be global to the app being instrumented.
-api.metrics.setGlobalMeterProvider(myServiceMeterProvider);
+const { DropAggregation, View } = require('@opentelemetry/sdk-metrics');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http')
 const sdk = new opentelemetry.NodeSDK({
     traceExporter: new OTLPTraceExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({
+            headers: { "x-honeycomb-dataset": `${process.env.SERVICE}-metrics` },
+        }),
+        interval: 60000,
+    }),
+    views: [
+        // Filter this due to high volume and redundancy with http spans
+        new View({
+            aggregation: new DropAggregation(),
+            meterName: "@opentelemetry/instrumentation-http",
+        }),
+    ],
     instrumentations: [
         getNodeAutoInstrumentations({
             // we recommend disabling fs autoinstrumentation since it can be noisy
